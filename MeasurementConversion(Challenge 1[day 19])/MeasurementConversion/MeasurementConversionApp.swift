d2c859e11c26d081cd4ec62e80fe26a406c33e1a
Ahmet Suat CAN <111503450+AhmetSuatCan@users.noreply.github.com>
2024-02-18 05:05:22 +0300

    Add files via upload

---
+++ MeasurementConversion(Challenge 1[day 19])/MeasurementConversion/MeasurementConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasurementConversionApp.swift
+//  MeasurementConversion
+//
+//  Created by Ahmet on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MeasurementConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
