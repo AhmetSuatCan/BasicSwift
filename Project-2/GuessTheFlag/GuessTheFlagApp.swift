b03fafc36cbf478e44751bcb1db53303280a6f09
Ahmet Suat CAN <111503450+AhmetSuatCan@users.noreply.github.com>
2024-02-21 04:41:25 +0300

    Add files via upload

---
+++ Project-2/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ahmet on 18.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
