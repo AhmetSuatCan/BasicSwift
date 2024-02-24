c0d06bd4aee52eab968c0d2fc07ef09429a155c9
Ahmet Suat CAN <111503450+AhmetSuatCan@users.noreply.github.com>
2024-02-25 02:36:47 +0300

    Add files via upload

---
+++ Project-4/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ahmet on 25.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
