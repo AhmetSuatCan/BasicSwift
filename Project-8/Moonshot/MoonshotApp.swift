4d4040f92f5b23c0241d8748a7fb979712856360
Ahmet Suat CAN <suatahmet@hotmail.com>
2024-03-03 12:13:04 +0300

    adding Project-8 files

---
+++ Project-8/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ahmet on 1.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
